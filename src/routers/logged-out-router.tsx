d26a18666256c53dd898a4beeb139d95077361a3
myunghyun_yang <shiningy84@gmail.com>
2021-03-21 16:49:57 +0900

    15.4 Form Design

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Route, Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import { CreateAccount } from "../pages/create-account";
 import { Login } from "../pages/login";
 
