74f7e6dd489379b3cca7e7f652b2612887e6f9ea
myunghyun_yang <shiningy84@gmail.com>
2021-03-27 17:12:38 +0900

    17.7 Code Challenge(category page implementation)

--- src/pages/client/category.tsx
+++ src/pages/client/category.tsx
@@ -1,6 +1,9 @@
 import { gql, useQuery } from "@apollo/client";
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { useHistory, useParams } from "react-router-dom";
+import { Restaurant } from "../../components/restaurant";
 import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
 import { category, categoryVariables } from "../../__generated__/category";
 
@@ -26,19 +29,99 @@ interface ICategoryParams {
   slug: string;
 }
 
+interface IFormProps {
+  searchTerm: string;
+}
+
 export const Category = () => {
+  const [page, setPage] = useState(1);
   const params = useParams<ICategoryParams>();
   const { data, loading } = useQuery<category, categoryVariables>(
     CATEGORY_QUERY,
     {
       variables: {
         input: {
-          page: 1,
+          page,
           slug: params.slug,
         },
       },
     }
   );
   console.log(data);
-  return <h1>Category</h1>;
+  const onNextPageClick = () => setPage((current) => current + 1);
+  const onPrevPageClick = () => setPage((current) => current - 1);
+  const { register, handleSubmit, getValues } = useForm<IFormProps>();
+  const history = useHistory();
+  const onSearchSubmit = () => {
+    const { searchTerm } = getValues();
+    history.push({
+      pathname: "/search",
+      search: `?term=${searchTerm}`,
+    });
+  };
+
+
+  return  (
+    <div>
+      <Helmet>
+        <title>Category | Nuber Eats</title>
+      </Helmet>
+      <form
+        onSubmit={handleSubmit(onSearchSubmit)}
+        className="bg-gray-800 w-full py-40 flex items-center justify-center"
+      >
+        <input
+          ref={register({ required: true, min: 3 })}
+          name="searchTerm"
+          type="Search"
+          className="input rounded-md border-0 w-3/4 md:w-3/12"
+          placeholder="Search restaurants..."
+        />
+      </form>
+      {!loading && (
+        <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
+          <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+            {data?.category?.restaurants?.map((restaurant) => (
+              <Restaurant
+                key={restaurant.id}
+                id={restaurant.id + ""}
+                coverImg={restaurant.coverImg}
+                name={restaurant.name}
+                categoryName={restaurant.category?.name}
+              />
+            ))}
+          </div>
+          <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
+            {page > 1 ? (
+              <button
+                onClick={onPrevPageClick}
+                className="focus:outline-none font-medium text-2xl"
+              >
+                &larr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+            <span>
+              Page {page} of {data?.category.totalPages}
+            </span>
+            {page !== data?.category.totalPages ? (
+              <button
+                onClick={onNextPageClick}
+                className="focus:outline-none font-medium text-2xl"
+              >
+                &rarr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
+function getValues(): { searchTerm: any; } {
+  throw new Error("Function not implemented.");
+}
+
