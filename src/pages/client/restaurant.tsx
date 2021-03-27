00ff5107848c6ff45768daa77373aa2fb1f2bc3d
myunghyun_yang <shiningy84@gmail.com>
2021-03-27 17:41:49 +0900

    17.9 Restaurant part 2

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { RESTAURANT_FRAGMENT } from "../../fragments";
-import { restaurant, restaurantVariables } from "../../__generated__/restaurant";
+import {
+  restaurant,
+  restaurantVariables,
+} from "../../__generated__/restaurant";
+import { Helmet } from "react-helmet-async";
 
 const RESTAURANT_QUERY = gql`
   query restaurant($input: RestaurantInput!) {
@@ -33,5 +37,27 @@ export const Restaurant = () => {
       },
     }
   );
-  return <h1>Restaurant</h1>;
+  return (
+    <div>
+      <Helmet>
+        <title>{data?.restaurant.restaurant?.name || ""} | Nuber Eats</title>
+      </Helmet>
+      <div
+        className="bg-gray-800 bg-center bg-cover py-24"
+        style={{
+          backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})`,
+        }}
+      >
+        <div className="bg-white w-3/12 py-8 pl-48">
+          <h4 className="text-4xl mb-3">{data?.restaurant.restaurant?.name}</h4>
+          <h5 className="text-sm font-light mb-2">
+            {data?.restaurant.restaurant?.category?.name}
+          </h5>
+          <h6 className="text-sm font-light">
+            {data?.restaurant.restaurant?.address}
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
 };
