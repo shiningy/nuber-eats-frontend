de44e9a6d7fcaa65c8d04aa0e1b489728e4404c1
myunghyun_yang <shiningy84@gmail.com>
2021-03-28 12:54:28 +0900

    18.4 Testing Header and 404

---
+++ src/pages/__tests__/404.spec.tsx
@@ -0,0 +1,21 @@
+import { render, waitFor } from "@testing-library/react";
+import React from "react";
+import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter as Router } from "react-router-dom";
+import { isIterationStatement } from "typescript";
+import { NotFound } from "../404";
+
+describe("<NotFound />", () => {
+  it("renders OK", async () => {
+    render(
+      <HelmetProvider>
+        <Router>
+          <NotFound />
+        </Router>
+      </HelmetProvider>
+    );
+    await waitFor(() => {
+      expect(document.title).toBe("Not Found | Nuber Eats");
+    });
+  });
+});
