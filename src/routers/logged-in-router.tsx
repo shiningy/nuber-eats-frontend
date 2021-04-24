37ae50b45041f04c6cf86ec8f72d48614b67d5e1
myunghyun_yang <shiningy84@gmail.com>
2021-04-24 17:09:21 +0900

    20.8 Create Dish part 1

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -12,7 +12,9 @@ import { Category } from "../pages/client/category";
 import { Restaurant } from "../pages/client/restaurant";
 import { Restaurants } from "../pages/client/restaurants";
 import { Search } from "../pages/client/search";
+import { AddDish } from "../pages/owner/add-dish";
 import { AddRestaurant } from "../pages/owner/add-restaurants";
+import { MyRestaurant } from "../pages/owner/my-restaurant";
 import { MyRestaurants } from "../pages/owner/my-restaurants";
 import { ConfirmEmail } from "../pages/user/confirm-email";
 import { EditProfile } from "../pages/user/edit-profile";
@@ -65,6 +67,8 @@ const commonRoutes = [
 const restaurantRoutes = [
   { path: "/", component: <MyRestaurants /> },
   { path: "/add-restaurant", component: <AddRestaurant /> },
+  { path: "/restaurants/:id", component: <MyRestaurant /> },
+  { path: "/restaurants/:id/add-dish", component: <AddDish /> },
 ];
 
 export const LoggedInRouter = () => {
