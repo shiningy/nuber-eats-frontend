dce68328d755741a648acca38f284ae0a596e4e7
myunghyun_yang <shiningy84@gmail.com>
2021-05-01 23:29:25 +0900

    23.6 Driver Dashboard part 2

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,29 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact, { Position } from "google-map-react";
 
+interface ICoords {
+  lat: number;
+  lng: number;
+}
+
 export const Dashboard = () => {
-  const onWatchSuccess = (coord: Position) => {
-    console.log(coord);
+  const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
+  // @ts-ignore
+  const onSuccess = ({ coords: { latitude, longitude } }: Position) => {
+    setDriverCoords({ lat: latitude, lng: longitude });
+  };
+  // @ts-ignore
+  const onError = (error: PositionError) => {
+    console.log(error);
   };
   useEffect(() => {
-    // navigator.geolocation.watchPosition(onWatchSuccess);
+    navigator.geolocation.watchPosition(()=> onSuccess, onError, {
+      enableHighAccuracy: true,
+    });
   }, []);
+  const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+  };
   return (
     <div>
       <div
         className="overflow-hidden"
-        style={{ width: window.innerWidth, height: "95vh"}}
+        style={{ width: window.innerWidth, height: "95vh" }}
       >
         <GoogleMapReact
-          defaultZoom={10}
-          draggable={false}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={onApiLoaded}
+          defaultZoom={15}
+          draggable={true}
           defaultCenter={{
-            lat: 59.95,
-            lng: 30.33,
+            lat: 36.58,
+            lng: 125.95,
           }}
-          bootstrapURLKeys={{key: "AIzaSyAu5o500KGYEH4LnRdBh2dCbXxUNBs1Ic0"}}
+          bootstrapURLKeys={{ key: "AIzaSyAu5o500KGYEH4LnRdBh2dCbXxUNBs1Ic0" }}
         ></GoogleMapReact>
       </div>
     </div>
-  )
+  );
 };
