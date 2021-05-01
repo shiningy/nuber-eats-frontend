3413fe271ec7122318c8a79a43ca8bac828c9ece
myunghyun_yang <shiningy84@gmail.com>
2021-05-01 23:54:52 +0900

    23.7 Driver Dashboard part 3

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -8,6 +8,8 @@ interface ICoords {
 
 export const Dashboard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
+  const [map, setMap] = useState<any>();
+  const [maps, setMaps] = useState<any>();
   // @ts-ignore
   const onSuccess = ({ coords: { latitude, longitude } }: Position) => {
     setDriverCoords({ lat: latitude, lng: longitude });
@@ -17,30 +19,46 @@ export const Dashboard = () => {
     console.log(error);
   };
   useEffect(() => {
-    navigator.geolocation.watchPosition(()=> onSuccess, onError, {
+    navigator.geolocation.watchPosition(() => onSuccess, onError, {
       enableHighAccuracy: true,
     });
   }, []);
+  useEffect(() => {
+    if (map && maps) {
+      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    }
+  }, [driverCoords.lat, driverCoords.lng]);
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
     map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    setMap(map);
+    setMaps(maps);
   };
   return (
     <div>
       <div
         className="overflow-hidden"
-        style={{ width: window.innerWidth, height: "95vh" }}
+        style={{ width: window.innerWidth, height: "50vh" }}
       >
         <GoogleMapReact
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={onApiLoaded}
-          defaultZoom={15}
-          draggable={true}
+          defaultZoom={16}
+          draggable={false}
           defaultCenter={{
             lat: 36.58,
-            lng: 125.95,
+            lng: 126.95,
           }}
           bootstrapURLKeys={{ key: "AIzaSyAu5o500KGYEH4LnRdBh2dCbXxUNBs1Ic0" }}
-        ></GoogleMapReact>
+        >
+          <div
+            // @ts-ignore
+            lat={driverCoords.lat}
+            lng={driverCoords.lng}
+            className="text-lg"
+          >
+            🚖
+          </div>
+        </GoogleMapReact>
       </div>
     </div>
   );
